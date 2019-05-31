c4fb5aa6710574e08222ead1ca8e299c98270690
Alex Nahimana <alex.nahimana1@gmail.com>
2019-05-31 00:48:25 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -6,9 +6,14 @@ def array_with_two_elements
   my_array = ["string", 1]
 end
 
-def first_element(arr)
-  arr.first
+def first_element(arg)
+  arg.first
 end
 
-def third_element(arr)
+def third_element(arg)
+  arg[2]
+end
+
+def first_element(arg)
+  arg.last
 end
